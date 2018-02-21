213a22fd5d7581823eae49dfc12649a091a856e1
NeatNit <What@is.this>
2018-02-21 02:10:34 +0200

    style updates, and implemented copy of dimensional scale

--- lua/weapons/gmod_tool/stools/effectresizer.lua
+++ lua/weapons/gmod_tool/stools/effectresizer.lua
@@ -9,23 +9,21 @@ if CLIENT then
 	language.Add("tool.effectresizer.0", "Left Click to apply, Right Click to copy, Reload to reset")
 end
 
-TOOL.ClientConVar[ "scale" ] = 1
-TOOL.ClientConVar[ "scalex" ] = 1
-TOOL.ClientConVar[ "scaley" ] = 1
-TOOL.ClientConVar[ "scalez" ] = 1
+TOOL.ClientConVar.scale = 1
+TOOL.ClientConVar.scalex = 1
+TOOL.ClientConVar.scaley = 1
+TOOL.ClientConVar.scalez = 1
 
 
 if SERVER then util.AddNetworkString("EffectResizeEntityMatrix") end
 if CLIENT then
 	net.Receive("EffectResizeEntityMatrix", function()
 		local effect = net.ReadEntity()
-		if !IsValid(effect) then return end
+		if not IsValid(effect) then return end
 
-		local mat = Matrix()
-		
 		timer.Simple(0.25, function()	-- gotta delay this so that the NWVector arrives, how shitty is that?
 			local scale = effect:GetNWVector("RenderMultiplyMatrixScale", Vector( 1, 1, 1 ))
-			
+
 			local nochange = (scale.x == 1 and scale.y == 1 and scale.z == 1)	-- disable matrix scaling altogether
 
 			local xflat = (scale.x == 0) and 1 or 0
@@ -47,17 +45,18 @@ end
 
 function TOOL:LeftClick( trace )
 	local ent = trace.Entity
-	if !IsValid(ent) then return false end
-	if ent:GetClass() != "prop_effect" then return false end
+	if not IsValid(ent) then return false end
+	if ent:GetClass() ~= "prop_effect" then return false end
 	if CLIENT then return true end
 
+	-- Only the server has AttachedEntity
 	local effect = ent.AttachedEntity
-	if !IsValid(effect) then return false end -- sadly can't be synced with client
+	if not IsValid(effect) then return false end -- sadly can't be synced with client, but it should never really happen anyway
 
 	effect:SetModelScale(self:GetClientNumber("scale"))
 
 	local scale = Vector(self:GetClientNumber("scalex"), self:GetClientNumber("scaley"), self:GetClientNumber("scalez"))
-	
+
 	effect:SetNWVector("RenderMultiplyMatrixScale", scale)
 
 	net.Start("EffectResizeEntityMatrix")
@@ -69,28 +68,36 @@ end
 
 function TOOL:RightClick( trace )
 	local ent = trace.Entity
-	if !IsValid(ent) then return false end
-	if ent:GetClass() != "prop_effect" then return false end
+	if not IsValid(ent) then return false end
+	if ent:GetClass() ~= "prop_effect" then return false end
 	if CLIENT then return true end
 
+	-- Only the server has AttachedEntity
 	local effect = ent.AttachedEntity
-	if !IsValid(effect) then return false end -- sadly can't be synced with client
+	if not IsValid(effect) then return false end
 
 	self:GetOwner():ConCommand( "effectresizer_scale " .. effect:GetModelScale())
+
+	local scale = effect:GetNWVector("RenderMultiplyMatrixScale", Vector( 1, 1, 1 ))
+	self:GetOwner():ConCommand( "effectresizer_scalex " .. scale.x)
+	self:GetOwner():ConCommand( "effectresizer_scaley " .. scale.y)
+	self:GetOwner():ConCommand( "effectresizer_scalez " .. scale.z)
+
 	return true
 end
 
 function TOOL:Reload( trace )
 	local ent = trace.Entity
-	if !IsValid(ent) then return false end
-	if ent:GetClass() != "prop_effect" then return false end
+	if not IsValid(ent) then return false end
+	if ent:GetClass() ~= "prop_effect" then return false end
 	if CLIENT then return true end
 
+	-- Only the server has AttachedEntity
 	local effect = ent.AttachedEntity
-	if !IsValid(effect) then return false end -- sadly can't be synced with client
+	if not IsValid(effect) then return false end
 
 	effect:SetModelScale(1)
-	
+
 	local scale = Vector(1, 1, 1)
 	effect:SetNWVector("RenderMultiplyMatrixScale", scale)
 
